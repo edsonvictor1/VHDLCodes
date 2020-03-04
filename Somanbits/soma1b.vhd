7d39a40382ae45a95fca4c67049df57509a0a3f4
Edson Victor <48814904+edsonvictor1@users.noreply.github.com>
2020-03-04 08:37:31 -0300

    Add files via upload

---
+++ Somanbits/soma1b.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity soma1b is
+port(a,b,cin: in bit;
+		s, cout: out bit);
+end soma1b;
+
+architecture soma of soma1b is
+signal x: bit;
+
+begin
+
+x <= a xor b;
+s <= cin xor x;
+cout <= (a and b) or (cin and x);
+
+end soma;
