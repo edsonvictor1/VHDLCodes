eb95d3a772e5fb409f81967d807321505251ef0f
Edson Victor <48814904+edsonvictor1@users.noreply.github.com>
2020-10-05 11:03:13 -0300

    Update 74hc595.vhd

--- CI74HC595/74hc595.vhd
+++ CI74HC595/74hc595.vhd
@@ -9,27 +9,26 @@ end ci74hc595;
 
 architecture ci of ci74hc595 is 
 
-signal serial_out:std_logic_vector(7 downto 0):=(others => '0'); --sinal S que sai de um ff e entra no outro, do primeiro ate o ultimo ff
-
-signal parallel_out:std_logic_vector(7 downto 0):=(others => '0');-- sinal R que ai de um ff e entra no outro, do primeiro ate o ultimo ff
+signal serial_out:std_logic_vector(7 downto 0) := (others => '0');
+signal parallel_out:std_logic_vector(7 downto 0) := (others => '0'); 
 
 begin
 
   r: process(srclr,srclk,rclk)  
 		begin
-            if(srclr='0') then --reset prioridade maior, se for 0 o sr recebe 0 em todos 
-				serial_out<=(others => '0');
-          elsif (srclk'event and srclk = '1') then --se o reset for 1 e ocorrer um evento no clock (que entra em todos da primeira coluna de ff), o sinal sr (na posição n-2)  recebe o sinal anterior na posição anterior
-          serial_out(6 downto 0)<= serial_out(7 downto 1);
-				serial_out(7)<=ser; -- o primeiro sr recebe ser 
+			
+          if(srclr='0') then
+		     serial_out <= (others => '0');
+          elsif (srclk'event and srclk='1') then 
+          serial_out(6 downto 0) <= serial_out(7 downto 1);
+				serial_out(7) <= ser;
 			end if;
-        if(rclk'event and rclk = '1') then -- se o clock da segunda coluna tiver em 0, o sinal R (div) recebe o sinal S (reg)
-			parallel_out <=serial_out;
+        if(rclk'event and rclk='1') then 
+			parallel_out <= serial_out;
 		end if;
-		
-	q<= (parallel_out) when oe = '0' else 
-		q := (others => 'Z');
-	qh<= serial_out(0);
+	q <= parallel_out when oe ='0' else 
+		(others => 'Z');
+	qh <= serial_out(0);
 	end process;
 	
 end ci;
